36a6574bb4ed0e255238a47f27887be34c1c4d18
stovag <p18stog@ionio.gr>
2018-12-21 22:24:09 +0200

    added variables in js

--- Save Choices.js
+++ Save Choices.js
@@ -1,23 +1,28 @@
-function Save_choices1(){
-
+function Save_choices1(choice){
+  c1 = choice;
+  return c1;
 }
 
 
-function Save_choices2(){
-
+function Save_choices2(choice){
+  c2 = choice;
+  return c2;
 }
 
 
-function Save_choices3(){
-
+function Save_choices3(choice){
+  c3 = choice;
+  return c3;
 }
 
 
-function Save_choices3b(){
-
+function Save_choices3b(choice){
+  c3b = choice;
+  return c3b;
 }
 
 
-function Save_choices4(){
-
+function Save_choices4(choice){
+  c4 = choice;
+  return c4;
 }
